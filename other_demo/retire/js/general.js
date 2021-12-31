cdd103ff2ce3c1f1eb824de58be65af70633f3b7
YH-kuo <akankeita1216@hotmail.com>
2021-12-31 14:04:29 +0800

    Fix stories to kol

--- other_demo/retire/js/general.js
+++ other_demo/retire/js/general.js
@@ -12,6 +12,9 @@ $(document).ready(function () {
       $('.Slides-indicator a.active').removeClass('active');
       $($('.Slides-indicator a')[index - 2]).addClass('active');
 
+      var slides_top = $("#"+dataPage).offset().top;
+      $('html,body').animate({scrollTop:slides_top},100);
+
       $('#Receive_btn').attr('data-gtm-event', dataPage + (index - 1) + '-N');
       $('#Receive_btn').data('gtm-event', dataPage + (index - 1) + '-N');
       $('#Receive_mobile_btn').attr('data-gtm-event', dataPage + (index - 1) + '-N');
@@ -32,12 +35,15 @@ $(document).ready(function () {
       $slides.attr('active-item', index + 1);
       $('.Slides-indicator a.active').removeClass('active');
       $($('.Slides-indicator a')[index]).addClass('active');
+      
+      var slides_top = $("#"+dataPage).offset().top;
+      $('html,body').animate({scrollTop:slides_top},100);
 
       $('#Receive_btn').attr('data-gtm-event', dataPage + (index + 1) + '-N');
       $('#Receive_btn').data('gtm-event', dataPage + (index + 1) + '-N');
       $('#Receive_mobile_btn').attr('data-gtm-event', dataPage + (index + 1) + '-N');
       $('#Receive_mobile_btn').data('gtm-event', dataPage + (index + 1) + '-N');
-      gtmClickTrack('view-'+dataPage+(index+1))
+      gtmClickTrack('view-'+dataPage+(index+1));
     }
     showReadMore();//閱讀更多顯示
   });
@@ -89,6 +95,7 @@ function sectionShow() {
 }
 
 function changeSlide(element, index) {
+  let dataPage = $('.dataPage').attr('id');
   let $slides = $(element).closest('.Slides');
 
   $('.Slides-indicator a.active').removeClass('active');
@@ -96,8 +103,10 @@ function changeSlide(element, index) {
   if (index <= 4 && index >= 1) {
     $slides.attr('active-item', index);
     $($('.Slides-indicator a')[index - 1]).addClass('active');
-    $('#Receive_btn').attr('data-gtm-event', 'tips' + index + '-N');
-    $('#Receive_btn').data('gtm-event', 'tips' + index + '-N');
+    if(dataPage == 'Tips'){
+      $('#Receive_btn').attr('data-gtm-event', 'tips' + index + '-N');
+      $('#Receive_btn').data('gtm-event', 'tips' + index + '-N');
+    }
   }
   showReadMore();//閱讀更多顯示
 }
@@ -139,9 +148,12 @@ function checkHash() {
     var hash = window.location.hash.substring(1);
     var dataPage = $('.dataPage').attr('id');
 
-    console.log('checkHash',hash);
     if (hash && hash.indexOf(dataPage) >= 0) {
-      var index = Number(hash.substr(4,1));
+      if(dataPage == 'Tips'){
+        var index = Number(hash.substr(4,1));
+      }else if(dataPage == 'kol'){
+        var index = Number(hash.substr(3,1));
+      }
 
       if (!isNaN(index)) {
         changeSlide($('.Slides-indicator'), index);
@@ -173,35 +185,42 @@ function toggleFooter () {
 //展開閱讀更多 
 function openMore(){
  let activeItem = $(document.getElementsByClassName('Slides')).attr('active-item');
- let slidesItem = document.getElementsByClassName('Slides-item');
+ let overflowBox = document.getElementsByClassName('overflow-box');
+ let slidesItem = document.getElementsByClassName('inner-box');
  let readMoreBtn = document.getElementsByClassName('read-more');
  let storyBodyHeight = slidesItem[activeItem-1].offsetHeight;
- let storiesBox = document.getElementById('stories');
- $(storiesBox).attr('style' , 'min-height: calc('+ storyBodyHeight +'px + 120px);');
+ let kolBox = document.getElementById('kol');
+ $(overflowBox).attr('style','max-height: initial;margin-bottom: 0;');
+ $(kolBox).attr('style' , 'min-height: calc('+ storyBodyHeight +'px + 400px);');
  $(readMoreBtn).hide();
 }
 //點擊左右收合
 function closeMore(){
- let storiesBox = document.getElementById('stories');
- $(storiesBox).attr('style' , '');
+ let kolBox = document.getElementById('kol');
+ let overflowBox = document.getElementsByClassName('overflow-box');
+ $(kolBox).attr('style' , '');
+
+ let navHeight = $('.navbar').innerHeight();
+ let windowHeight = $(window).innerHeight();
+ let sortyHeaderHeight = $('.story-header').innerHeight();
+ let fix = 220;
+ let overflowHeight = windowHeight - navHeight - sortyHeaderHeight - fix;
+ $(overflowBox).attr('style','max-height: '+overflowHeight+'px;margin-bottom: 2rem;');
 }
 //閱讀更多顯示
 function showReadMore(){
   var dataPage = $('.dataPage').attr('id');
-  if(dataPage == 'product'){
-  }else{
+  if(dataPage == 'kol'){
     closeMore()
     let activeItem = $(document.getElementsByClassName('Slides')).attr('active-item');
-    let slidesItem = document.getElementsByClassName('Slides-item');
+    let slidesItem = document.getElementsByClassName('inner-box');
     let storyBodyHeight = slidesItem[activeItem-1].offsetHeight;
     let readMoreBtn = document.getElementsByClassName('read-more');
-    let windowHeight = $(window).height()-54-40;
-    console.log(storyBodyHeight,windowHeight);
-    if(storyBodyHeight > windowHeight){
+    if(storyBodyHeight > 430){
       $(readMoreBtn).show();
     }else{
       $(readMoreBtn).hide();
     }
 
-  }
+  }else{}
 }
